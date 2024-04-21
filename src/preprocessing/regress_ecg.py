08b09fcaa8c365179ab2cc81d36101890dd1056a
Giuseppe Lai <g.lai@braincontrol.eu>
2024-04-21 18:57:50 +0100

    Add artefact ccorrection and regress ECG

--- src/preprocessing/regress_ecg.py
+++ src/preprocessing/regress_ecg.py
@@ -22,6 +22,8 @@ def regress_ecg(ecg_data_raw: np.ndarray, epochs: mne.Epochs) -> np.ndarray:
     # extract the R-peaks events from the whole recording (make sure the data is preprocessed)
     r_peaks = nk.ecg.ecg_findpeaks(ecg_data_raw, sampling_rate=epochs.info['sfreq'], show=False)['ECG_R_Peaks']
 
+    events = epochs.events # the event array contaning the timing (col1) and event id (col3)
+
     # Get ECG and EEG epochs data
     ecg_data = epochs.get_data('ecg') # the ECG epochs
     eeg_data = epochs.get_data('eeg') # the EEG epochs
@@ -40,14 +42,13 @@ def regress_ecg(ecg_data_raw: np.ndarray, epochs: mne.Epochs) -> np.ndarray:
 
     # Get time vector and events array
     time = epochs.times # the 1D time vector (given by mne.Epochs object)
-    events = epochs.events # the event array contaning the timing (col1) and event id (col3)
-
-    rpeaks_events = []
+    
+    r_peaks_events = []
     for n_epoch in range(0, n_epochs):
 
         # Get the relevant R-peaks timing for the current epoch
         r_peaks_temp = r_peaks[np.where(r_peaks >= events[n_epoch, 0])[0][0]] 
-        rpeaks_events.append(r_peaks_temp) # Store the R-peaks events
+        r_peaks_events.append(r_peaks_temp) # Store the R-peaks events
 
         tzero = np.where(time == 0.0)[0][0] # get index of time zero (cue presentation)
         # Calculate distance from cue presentation to R-peak
@@ -91,4 +92,7 @@ def regress_ecg(ecg_data_raw: np.ndarray, epochs: mne.Epochs) -> np.ndarray:
         mean_errors[n_epoch, :] =  np.mean(error, axis=1)
         errors[n_epoch, :, :] = error
 
-    return regressed_eeg_epochs
+
+    
+
+    return regressed_eeg_epochs, r_peaks_events
